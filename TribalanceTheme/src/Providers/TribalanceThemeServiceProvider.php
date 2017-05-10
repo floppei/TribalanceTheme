c1df37157f1f50bbb0be3e06c8a9e9220c85a126
Florian Hanke <florian@hankeonline.net>
2017-05-10 15:53:25 +0200

    testcommit TBA Theme

--- TribalanceTheme/src/Providers/TribalanceThemeServiceProvider.php
+++ TribalanceTheme/src/Providers/TribalanceThemeServiceProvider.php
@@ -14,4 +14,15 @@ class TribalanceThemeServiceProvider extends ServiceProvider
     {
 
     }
+
+    /**
+     * Boot a template for the footer that will be displayed in the template plugin instead of the original footer.
+     */
+    public function boot(Twig $twig, Dispatcher $eventDispatcher)
+    {
+        $eventDispatcher->listen('IO.init.templates', function(Partial $partial)
+        {
+            $partial->set('footer', 'Theme::content.TribalanceThemeFooter');
+        }, 0);
+    }
 }
